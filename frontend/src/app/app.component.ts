56e74b3fa5fe23fb7dd864437b1b33a4a77784e7
Cezary Bober <ceski23@gmail.com>
2025-04-12 17:00:44 +0200

    feat(map): map

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoComponent } from '../components/logo/logo.component';
 import { ClinicListComponent } from '../components/clinic-list/clinic-list.component';
+import environment from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,14 @@ import { ClinicListComponent } from '../components/clinic-list/clinic-list.compo
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  showApp = signal(false)
+
+  ngOnInit(): void {
+    const script = document.createElement('script')
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}`
+    script.onload = () => this.showApp.set(true)
+    
+    document.head.appendChild(script)
+  }
+}
