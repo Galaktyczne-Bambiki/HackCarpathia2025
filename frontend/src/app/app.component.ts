41b566eaf15f16b36fa786390eb833bba4a44487
Magda98 <magdakochman7@gmail.com>
2025-04-12 16:26:14 +0200

    add clinic list component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoComponent } from '../components/logo/logo.component';
+import { ClinicListComponent } from '../components/clinic-list/clinic-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LogoComponent, LogoComponent],
+  imports: [RouterOutlet, LogoComponent, LogoComponent, ClinicListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
